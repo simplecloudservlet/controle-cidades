c423c80c316fa4404b8d97cc3d9fcdf15e63977b
Lucio Agostinho Rocha <outrosdiasvirao@gmail.com>
2023-06-20 18:51:59 -0700

    ngIf

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+//Inicio Locale
+import { LOCALE_ID, enableProdMode } from '@angular/core';
+
+platformBrowserDynamic()
+   .bootstrapModule(AppModule, {
+      providers: [{provide: LOCALE_ID, useValue: 'pt-BR' }],
+})
+.catch((err) => console.error(err));
+//Fim locale
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
